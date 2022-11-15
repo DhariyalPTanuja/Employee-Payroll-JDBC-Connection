f825c2ee5a72b1d3767c8567a05d88dade8c08ba
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-16 04:08:55 +0530

    database connection

---
+++ EmployeePayrollDataBase.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class EmployeePayrollDataBase {
+
+	public static void main(String[] args) {
+		System.out.println(" DataBase Connection ");
+
+	}
+}
