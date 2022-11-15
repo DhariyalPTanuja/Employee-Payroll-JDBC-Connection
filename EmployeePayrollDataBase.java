1e670e3a915dc19682a8c5cce7273e0dedec006e
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-16 04:13:23 +0530

    driver loaded

--- EmployeePayrollDataBase.java
+++ EmployeePayrollDataBase.java
@@ -6,7 +6,34 @@ import java.util.Enumeration;
 public class EmployeePayrollDataBase {
 
 	public static void main(String[] args) {
-		System.out.println(" DataBase Connection ");
+		String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll";
+		String userName = "root";
+		String password = "root";
+		Connection conn;
 
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Loades!!");
+
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("can not find the driver in the classpath:", e);
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database: " + jdbcURL);
+			conn = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is sucessfull!!! " + conn);
+		} catch (Exception e) {
+
+		}
 	}
-}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
+		}
+	}
+	}
+
