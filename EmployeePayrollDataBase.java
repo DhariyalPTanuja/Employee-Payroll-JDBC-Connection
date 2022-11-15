bedbc1a553f0fc47ca597f98714f4aad569c9003
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-16 04:43:11 +0530

    execute update query

--- EmployeePayrollDataBase.java
+++ EmployeePayrollDataBase.java
@@ -8,14 +8,14 @@ import java.util.Enumeration;
 public class EmployeePayrollDataBase {
 
 	public static void main(String[] args) {
-		String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll";
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
 		String userName = "root";
 		String password = "root";
 		Connection conn;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Driver Loades!!");
-			
+
 		} catch (ClassNotFoundException e) {
 			throw new IllegalStateException("can not find the driver in the classpath:", e);
 		}
@@ -25,7 +25,7 @@ public class EmployeePayrollDataBase {
 			conn = DriverManager.getConnection(jdbcURL, userName, password);
 			System.out.println("Connection is sucessfull!!! " + conn);
 			Statement statement = conn.createStatement();
-			ResultSet resultSet = statement.executeQuery("select * from payroll_employee");
+			ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
 			while (resultSet.next()) {
 				System.out.print(resultSet.getInt(1) + " ");
 				System.out.print(resultSet.getString(2) + " ");
@@ -33,9 +33,17 @@ public class EmployeePayrollDataBase {
 				System.out.print(resultSet.getString(4) + " ");
 				System.out.print(resultSet.getString(5) + " ");
 				System.out.print(resultSet.getString(6) + " ");
+				System.out.print(resultSet.getString(7) + " ");
+				System.out.print(resultSet.getString(8) + " ");
+				System.out.print(resultSet.getDouble(9) + " ");
+				System.out.print(resultSet.getDouble(10) + " ");
+				System.out.print(resultSet.getDouble(11) + " ");
+				System.out.print(resultSet.getDouble(12) + " ");
+				System.out.print(resultSet.getDouble(13) + " ");
 				System.out.println();
 
 			}
+			statement.executeUpdate("update employee_payroll set basic_pay ='450000'  where name = 'Chetna'");
 		} catch (Exception e) {
 
 		}
@@ -48,5 +56,4 @@ public class EmployeePayrollDataBase {
 			System.out.println(" " + driverClass.getClass().getName());
 		}
 	}
-	}
-
+}
